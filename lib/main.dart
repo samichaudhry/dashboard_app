c5b38f76dd980b2f2e43b5e2fe115800e4aab631
samichaudhry <samichoudhry@outlook.com>
2022-03-17 21:25:58 +0500

    signup_login dialogs added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:ui';
 import 'package:dashboard_app/address_summary_section.dart';
 import 'package:dashboard_app/tab_panel.dart';
 import 'package:flutter/material.dart';
@@ -23,8 +24,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'App Name',
-      darkTheme: ThemeData.dark(),
-      theme: ThemeData(primarySwatch: Colors.blue),
+      darkTheme:
+          ThemeData(primarySwatch: Colors.purple, brightness: Brightness.dark),
+      theme: ThemeData(primarySwatch: Colors.purple),
       home: const MyHomePage(),
     );
   }
@@ -42,36 +44,177 @@ class _MyHomePageState extends State<MyHomePage>
   final BoxDecoration _boxDecoration = BoxDecoration(
       color: const Color.fromARGB(255, 181, 144, 187),
       borderRadius: BorderRadius.circular(10));
+  Widget _customButton(title, _onPressed) {
+    return SizedBox(
+      height: 48,
+      child: ElevatedButton(
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.black)),
+          onPressed: _onPressed,
+          child: Text(title)),
+    );
+  }
+
+  int users = 0;
+
+  Widget showSignupDiaglog() {
+    final List<TextEditingController> _userName =
+        List.generate(12, (index) => TextEditingController());
+    final TextEditingController _password = TextEditingController();
+
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Dialog(
+        backgroundColor: const Color.fromARGB(255, 181, 144, 187),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Container(
+          height: 400,
+          width: 500,
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            children: [
+              Expanded(
+                child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 100,
+                      childAspectRatio: 1.3,
+                      crossAxisSpacing: 50,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount: 12,
+                    itemBuilder: (ctx, index) {
+                      return TextFormField(
+                        maxLength: 1,
+                        controller: _userName[index],
+                        decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.all(8),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(4)),
+                          focusColor: Colors.black,
+                          hintText: "Word ${index + 1}",
+                          filled: true,
+                          fillColor: const Color.fromARGB(255, 218, 184, 223),
+                        ),
+                        onSaved: (value) {
+                          _userName[index].text = value!;
+                        },
+                      );
+                    }),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                          controller: _password,
+                          decoration: InputDecoration(
+                            hintText: "Password",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(4)),
+                            contentPadding: const EdgeInsets.all(8),
+                            filled: true,
+                            fillColor: const Color.fromARGB(255, 218, 184, 223),
+                          ),
+                          onSaved: (value) {
+                            _password.text = value!;
+                          }),
+                    ),
+                    const SizedBox(
+                      width: 50,
+                    ),
+                    Expanded(child: _customButton("Submit", () {}))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget showLoginDiaglog() {
+    final TextEditingController _password = TextEditingController();
+
+    return BackdropFilter(
+      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+      child: Dialog(
+        backgroundColor: const Color.fromARGB(255, 181, 144, 187),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Container(
+          height: 400,
+          width: 500,
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextFormField(
+                  controller: _password,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4)),
+                    contentPadding: const EdgeInsets.all(8),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 218, 184, 223),
+                  ),
+                  onSaved: (value) {
+                    _password.text = value!;
+                  }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(child: _customButton("Login", () {})),
+                  const SizedBox(
+                    width: 50,
+                  ),
+                  Expanded(
+                      child: _customButton("Signup", () {
+                    showSignupDiaglog();
+                  }))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       body: SafeArea(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.all(18.0),
-            child: Row(
-              children: [
-                //  Grid Layout
-                const Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: EdgeInsets.only(right: 10),
-                      child: AddressSummarySection(),
-                    )),
-                Expanded(
-                  flex: 6,
-                  child: Container(
-                      decoration: _boxDecoration, child: const TabPanel()),
-                )
-              ],
-            ),
-          ),
-        ),
+        child: (users == 0)
+            ? showLoginDiaglog()
+            : Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                alignment: Alignment.center,
+                child: Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Row(
+                    children: [
+                      //  Grid Layout
+                      const Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: EdgeInsets.only(right: 10),
+                            child: AddressSummarySection(),
+                          )),
+                      Expanded(
+                        flex: 6,
+                        child: Container(
+                            decoration: _boxDecoration,
+                            child: const TabPanel()),
+                      )
+                    ],
+                  ),
+                ),
+              ),
       ),
     );
   }
